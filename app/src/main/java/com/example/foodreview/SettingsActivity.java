6b29b8baeea83b02ca04459c7a0465e8b8274dd3
Markusstr <markus.strandman@gmail.com>
2019-06-28 17:29:52 +0300

    Added back button to settings page

--- app/src/main/java/com/example/foodreview/SettingsActivity.java
+++ app/src/main/java/com/example/foodreview/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.foodreview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbarsettings);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.settings_title));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
