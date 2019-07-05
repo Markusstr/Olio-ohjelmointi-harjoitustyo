152b97528972f140ec08c79643f813bc958d8cc8
Markusstr <markus.strandman@gmail.com>
2019-07-05 13:58:38 +0300

    Changed recyclerview in admin page

--- app/src/main/java/com/example/foodreview/Restaurant.java
+++ app/src/main/java/com/example/foodreview/Restaurant.java
@@ -4,29 +4,18 @@ import java.util.ArrayList;
 
 public class Restaurant {
     private String name;
-    private String id;
-    ArrayList<Restaurant> restaurantList = new ArrayList<>();
+    private String address;
 
-    private static Restaurant restaurant = new Restaurant();
-
-    public static Restaurant getInstance() { return restaurant; }
-
-    private Restaurant() {
-
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
     }
 
     public String getRestaurantName() {
         return name;
     }
 
-    public String getRestaurantId() {
-        return id;
-    }
-
-    public void newRestaurant(String newName, String newId) {
-        Restaurant newRestaurant = new Restaurant();
-        newRestaurant.name = newName;
-        newRestaurant.id = newId;
-        restaurantList.add(newRestaurant);
+    public String getRestaurantAddress() {
+        return address;
     }
 }
