2ff423ad051835ea002cd696a3ed48b1fce35ad5
Markusstr <markus.strandman@gmail.com>
2019-07-08 20:07:12 +0300

    Added user manager

---
+++ app/src/main/java/com/example/foodreview/AdminUserManager.java
@@ -0,0 +1,27 @@
+package com.example.foodreview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Objects;
+
+public class AdminUserManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_user_manager);
+
+        Toolbar toolbar = findViewById(R.id.toolbaradmin);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getResources().getString(R.string.adminusermanager_title));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
