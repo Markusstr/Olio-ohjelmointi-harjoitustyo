78d85d3804bc8cb94310500ea7ba51d99c1d1fd0
johan1954 <tomi.enberg@student.lut.fi>
2019-07-12 10:19:39 +0300

    Print to file class updated to a singleton.

--- app/src/main/java/com/example/foodreview/PrintToFile.java
+++ app/src/main/java/com/example/foodreview/PrintToFile.java
@@ -2,6 +2,23 @@ package com.example.foodreview;
 
 public class PrintToFile {
 
+    private PrintToFile instance = null;
 
+    private PrintToFile() {
 
+    }
+
+    PrintToFile getInstance() {
+        if (instance == null) {
+            instance = new PrintToFile();
+        }
+        else {
+            System.out.println("Instance already exists.");
+        }
+        return instance;
+    }
+
+    void executePrint(University university) {
+
+    }
 }
